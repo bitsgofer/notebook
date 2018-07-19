14958c632cacb861cb5ee3cb6eee4e5999563673
Tong Huu Khiem (Mark) <mark@bitsgofer.com>
2018-07-19 23:01:29 +0800

    notebook server

---
+++ cmd/notebook/server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+	"os"
+
+	"github.com/exklamationmark/glog"
+
+	"github.com/exklamationmark/notebook/internal/notebook"
+)
+
+func main() {
+	var root string
+	var addr string
+
+	flag.StringVar(&addr, "addr", ":8080", "HTTP addr")
+	flag.StringVar(&root, "root", "/var/www/notebook/", "root of notebook")
+
+	flag.Parse()
+	defer glog.Flush()
+
+	nb, err := notebook.NewNotebook(root)
+	if err != nil {
+		glog.Errorf("cannot create notebook from %q, err= %v", root, err)
+		os.Exit(1)
+	}
+
+	http.HandleFunc("/nb1", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		nb.RenderHTML(w)
+	})
+
+	glog.Warningf("listening on %s", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		glog.Errorf("failed to run http server, err= %v", err)
+		os.Exit(1)
+	}
+}

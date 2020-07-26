9a282bba0b20b19c0f14b69d25c7e810a0219820
exklamationmark <tonghuukhime@gmail.com>
2020-07-25 19:07:47 -0700

    tmp

--- Makefile
+++ Makefile
@@ -1,45 +1,83 @@
-_pkg=github.com/bitsgofer/notebook
+IMPORT_PATH=github.com/bitsgofer/notebook
+define GO_ENV
+	GOPROXY=https://proxy.golang.org,direct \
+	GOPRIVATE=github.com/bitsgofer/*
+endef
+GO=${GO_ENV}; go
+define GORELEASER
+	@echo "Using Github token: $$GITHUB_TOKEN";
+	docker run -v ${PWD}:/go/src/${IMPORT_PATH} \
+		-v /var/run/docker.sock:/var/run/docker.sock \
+		-w /go/src/${IMPORT_PATH} \
+		-e GITHUB_TOKEN \
+		-e DOCKER_USERNAME \
+		-e DOCKER_PASSWORD \
+		-e DOCKER_REGISTRY \
+		goreleaser/goreleaser:v0.139
+endef
 
-PKG=./...
-RUN=
-ifneq ("$(RUN)","")
-	_go_test_run_flag=-run $(RUN)
-endif
-GLOG=
+.mk_state:
+	mkdir -p .mk_state
 
-all: test build gen
-.PHONY: all
+# Empty target to record events: http://www.gnu.org/software/make/manual/make.html#Empty-Targets
+.mk_state/goreleaser.docker: .mk_state
+	docker pull goreleaser/goreleaser:v0.139
+	touch .mk_state/goreleaser.docker
 
-test:
-	go test -cover -v -race $(_pkg)/$(PKG) ${_go_test_run_flag} $(GLOG)
-	go vet $(_pkg)/$(PKG)
-.PHONY: test
-
-build:
-	mkdir -p build
-	go build -o build/notebook ${_pkg}/cmd/notebook
-.PHONY: build
-
-gen.assets:
-	minify assets/*.css > public_html/builtin.css
-	minify assets/*.js > public_html/builtin.js
-	cp assets/favicon.ico public_html/favicon.ico
-.PHONY: gen.assets
-
-gen: clean gen.assets
-	./build/notebook generate --post.dir=posts/ --html.dir=public_html/ --post.template=template.html
-.PHONY: gen
-
-serve.local:
-	sudo ./build/notebook serve --html.dir=public_html/
-.PHONY: serve.local
-
-run: build gen serve.local
-.PHONY: run
-
-clean:
-	rm -rf public_html/*/
-	rm -f public_html/*.css
-	rm -f public_html/*.js
-	rm -f public_html/*.ico
+# Phone targets: http://www.gnu.org/software/make/manual/make.html#Phony-Targets
 .PHONY: clean
+clean:
+	rm -rf bin
+
+
+.PHONY: test
+test:
+	$(GORELEASER) --snapshot --skip-publish --rm-dist
+
+# build:
+# 	$(GO) build -o bi
+# .PHONY build
+#
+# PKG=./...
+# RUN=
+# ifneq ("$(RUN)","")
+# 	_go_test_run_flag=-run $(RUN)
+# endif
+# GLOG=
+#
+# all: test build gen
+# .PHONY: all
+#
+# test:
+# 	go test -cover -v -race $(_pkg)/$(PKG) ${_go_test_run_flag} $(GLOG)
+# 	go vet $(_pkg)/$(PKG)
+# .PHONY: test
+#
+# build:
+# 	mkdir -p build
+# 	go build -o build/notebook ${_pkg}/cmd/notebook
+# .PHONY: build
+#
+# gen.assets:
+# 	minify assets/*.css > public_html/builtin.css
+# 	minify assets/*.js > public_html/builtin.js
+# 	cp assets/favicon.ico public_html/favicon.ico
+# .PHONY: gen.assets
+#
+# gen: clean gen.assets
+# 	./build/notebook generate --post.dir=posts/ --html.dir=public_html/ --post.template=template.html
+# .PHONY: gen
+#
+# serve.local:
+# 	sudo ./build/notebook serve --html.dir=public_html/
+# .PHONY: serve.local
+#
+# run: build gen serve.local
+# .PHONY: run
+#
+# clean:
+# 	rm -rf public_html/*/
+# 	rm -f public_html/*.css
+# 	rm -f public_html/*.js
+# 	rm -f public_html/*.ico
+# .PHONY: clean
--- cmd/notebook/main.go
+++ cmd/notebook/main.go
@@ -2,141 +2,30 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
 
-	"github.com/pkg/errors"
-	kingpin "gopkg.in/alecthomas/kingpin.v2"
-
-	"github.com/bitsgofer/notebook/internal/blog"
-	"github.com/bitsgofer/notebook/internal/middlewares/redirect"
-	"github.com/bitsgofer/notebook/internal/staticgen"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"k8s.io/klog/v2"
 )
 
-type domainsFlag []string
+var (
+	generate            = kingpin.Command("generate", "generate static blog")
+	generatePostDir     = generate.Flag("post-dir", "post directory").Default("posts").String()
+	generateTemplateDir = generate.Flag("template-dir", "templates directory").Default("templates").String()
+	generateHTMLDir     = generate.Flag("html-dir", "directory for resulting HTML/assets").Default("public_html").String()
 
-func (d *domainsFlag) String() string {
-	return fmt.Sprint(*d)
-}
-
-func (d *domainsFlag) Set(str string) error {
-	for _, domain := range strings.Split(str, ",") {
-		*d = append(*d, domain)
-	}
-
-	return nil
-}
-
-type config struct {
-	htmlDir string
-
-	// 	generate
-	postDir      string
-	postTemplate string
-
-	// server
-	adminEmail   string
-	domains      domainsFlag
-	production   bool
-	redirections redirect.Redirections
-}
+	server                  = kingpin.Command("server", "serve static blog")
+	serverEnableHTTPS       = server.Flag("https", "use HTTPS instead of HTTP").Default("true").Bool()
+	serverLetsEncryptEmail  = server.Flag("lets-encrypt-email", "email used with Let's Encrypt").Default("admin@example.com").String()
+	serverLetsEncryptDomain = server.Flag("lets-encrypt-domain", "comma-separated domains used with Let's Encrypt").Default("example.com,www.example.com").String()
+)
 
 func main() {
-	var c config
+	klog.InitFlags(nil)
 
-	a := kingpin.New(filepath.Base(os.Args[0]), "notebook application")
-	a.HelpFlag.Short('h')
-
-	a.Flag("html.dir", "output html directory").Default("public_html").
-		StringVar(&c.htmlDir)
-
-	gen := a.Command("generate", "generate HTML from posts")
-
-	gen.Flag("post.dir", "post directory").Default("posts").
-		StringVar(&c.postDir)
-
-	gen.Flag("post.template", "post template file").Default("template.html").
-		StringVar(&c.postTemplate)
-
-	server := a.Command("serve", "run blog server")
-
-	server.Flag("admin.email", "admin email for Let's Encrypt").Default("admin@example.com").
-		StringVar(&c.adminEmail)
-
-	server.Flag("domains", "domains to obtain TLS cert").Default("example.com").
-		SetValue(&c.domains)
-
-	server.Flag("production", "production mode (enable HTTPS)").Default("false").
-		BoolVar(&c.production)
-
-	server.Flag("redirect", "redirection options").Default("").
-		SetValue(&c.redirections)
-
-	// ----------------------------------------
-
-	cmd, err := a.Parse(os.Args[1:])
-	if err != nil {
-		fmt.Fprintln(os.Stderr, errors.Wrapf(err, "Error parsing commandline arguments"))
-		a.Usage(os.Args[1:])
-		os.Exit(2)
-	}
-	fmt.Printf("%#v\n", cmd)
-
-	switch cmd {
+	switch kingpin.Parse() {
 	case "generate":
-		if err := staticgen.Generate(c.postDir, c.postTemplate, c.htmlDir); err != nil {
-			fmt.Fprintln(os.Stderr, errors.Wrapf(err, "Error generating html"))
-			os.Exit(1)
-		}
-	case "serve":
-		fmt.Printf("%#v\n", c)
-		srv, err := blog.New(c.htmlDir, c.adminEmail, c.domains, blog.Redirect(c.redirections))
-		if err != nil {
-			fmt.Fprintln(os.Stderr, errors.Wrapf(err, "Cannot create server"))
-			os.Exit(1)
-		}
-
-		if !c.production {
-			runInDev(srv)
-			return
-		}
-		runInProd(srv)
-	}
-}
-
-func runInProd(srv *blog.Server) {
-	go func() {
-		httpSrv := &http.Server{
-			Addr:    ":80",
-			Handler: srv.HTTPRedirectHandler(),
-		}
-		if err := httpSrv.ListenAndServe(); err != nil {
-			fmt.Fprintln(os.Stderr, errors.Wrapf(err, "HTTP server failed"))
-			os.Exit(1)
-		}
-	}()
-
-	blogSrv := &http.Server{
-		Addr:      ":443",
-		Handler:   srv.BlogHandler(),
-		TLSConfig: srv.TLSConfig(),
-	}
-	if err := blogSrv.ListenAndServeTLS("", ""); err != nil {
-		fmt.Fprintln(os.Stderr, errors.Wrapf(err, "Blog server failed"))
-		os.Exit(1)
-	}
-}
-
-func runInDev(srv *blog.Server) {
-	blogSrv := &http.Server{
-		Addr:    ":80",
-		Handler: srv.BlogHandler(),
-	}
-	if err := blogSrv.ListenAndServe(); err != nil {
-		fmt.Fprintln(os.Stderr, errors.Wrapf(err, "blog server failed"))
-		os.Exit(1)
+	case "server":
 	}
 
+	fmt.Println("done")
 }
